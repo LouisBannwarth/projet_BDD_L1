8bee254773f15bd1fa551e6e8419be1a7661c268
BANNWARTH Louis <lbannwarth@uds-500444.ad.unistra.fr>
2017-10-05 17:42:22 +0200

    maj

--- Table.sql
+++ Table.sql
@@ -1,1 +1,10 @@
-CREATE TABLE 
+CREATE TABLE RECETTE
+(
+	idRecette number(6) PRIMARY KEY,
+	nom varchar2(40),
+	descriptif varchar2(150),
+	auteur varchar2(40),
+	difficulte varchar2(40),
+	
+)
+CREATE TABLE
